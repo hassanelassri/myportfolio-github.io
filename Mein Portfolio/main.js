591736da80d2f8d4128b95ff10d4b7fe74fa518a
Hassan elassri <166333542+hassanelassri@users.noreply.github.com>
2024-04-12 22:43:52 +0000

    Add files via upload

---
+++ Mein Portfolio/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Game Developer", "Social Media Marketer"], // Note: strings should be lowercase
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
